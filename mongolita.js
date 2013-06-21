53210b323831e145fb7eb909965f6a68767b75af
Ivan Povalyukhin <ivanpoval@gmail.com>
2013-06-21 13:13:46 -0700

    worked on the connect to mongodb component

--- mongolita.js
+++ mongolita.js
@@ -1,6 +1,25 @@
-load('./lib/mdb.indexes.js');
-load('./lib/mdb.operations.js');
+(function() {
+  this.default_host    = 'localhost';
+  this.default_port    = 27017;
+  this.default_db_name = 'mam';
 
-db = connect('localhost:10000/mam');
+  this.mongodb_connect = function mongodb_connect(host, port, db_name, callback_fn) {
+    this.default_db = connect(host + ':' + port);
+    this.working_db = this.default_db.getSisterDB(db_name);
 
-sister_db = db.getSisterDB('db_name');
+    callback_fn();
+  };
+})();
+
+mongodb_connect(
+  default_host,
+  default_port,
+  default_db_name,
+  function() {
+    print('Connected to ...' + this.working_db.getName());
+    print('`db.serverStatus();`');
+  }
+);
+
+// load('./lib/mdb.indexes.js');
+// load('./lib/mdb.operations.js');
