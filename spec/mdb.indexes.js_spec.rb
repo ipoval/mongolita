4ea44e5956346af23ed7333676b9f302eb221003
Ivan Povalyukhin <ivanpoval@gmail.com>
2013-07-11 20:15:00 -0700

    gracefuly kill the mongod process after the tests run

--- spec/mdb.indexes.js_spec.rb
+++ spec/mdb.indexes.js_spec.rb
@@ -1,9 +1,11 @@
 require_relative 'spec_helper'
 
 describe 'mdb.indexes.js' do
+  let(:lib_path) { File.expand_path('../../lib/mdb.indexes.js', __FILE__) }
+
   describe 'function ls_indexes_size_desc' do
     specify 'returns the list of index size per collection sorted by size desc' do
-      system "#{$server_settings.mdb_path}/mongo --eval 'db.help();'"
+      puts run_mongo 'typeof(ls_indexes_size_desc);'
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require 'fileutils'
 
 class ServerSettings < Hash
   def mdb_path
@@ -6,9 +7,11 @@ class ServerSettings < Hash
   end
 end
 
-$server_settings = ServerSettings.new
-$server_settings[:mdb_path] = File.join('/Users', ENV['USER'], 'bin/mongodb')
-$server_settings.freeze
+$db_settings = ServerSettings.new
+$db_settings[:mdb_path]      = File.join('/Users', ENV['USER'], 'bin/mongodb')
+$db_settings[:tmp_dbpath]    = '/tmp/mongolita'
+$db_settings[:tmp_mongolock] = File.join $db_settings[:tmp_dbpath], 'mongod.lock'
+$db_settings.freeze
 
 RSpec.configure do |conf|
   conf.order = 'random'
@@ -21,13 +24,20 @@ RSpec.configure do |conf|
   conf.expect_with :rspec, :stdlib
 
   conf.before(:all) {
-    system 'mkdir /tmp/mongolita/'
-    system "#{$server_settings.mdb_path}/mongod --dbpath /tmp/mongolita/ --logpath /dev/null --fork"
-  }
-
-  conf.before(:each) {
+    FileUtils.mkdir_p $db_settings[:tmp_dbpath]
+    system "#{$db_settings.mdb_path}/mongod --dbpath #{$db_settings[:tmp_dbpath]} --logpath /dev/null --fork"
   }
 
   conf.after(:all) {
+    Process.kill 9, File.read($db_settings[:tmp_mongolock]).strip.to_i
   }
 end
+
+module MongolitaTestHelpers
+  def run_mongo test_execution_str
+    cmd = %Q{#{$db_settings.mdb_path}/mongo --eval "load('#{lib_path}'); #{test_execution_str};"}
+    %x/#{cmd}/
+  end
+end
+
+include MongolitaTestHelpers
