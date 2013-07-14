a10673f87a85ea70d43fc64f1007034d67b6e962
Ivan Povalyukhin <ivanpoval@gmail.com>
2013-07-14 02:31:03 -0700

    test the total_index_size_per_collection function

--- spec/mdb.indexes.js_spec.rb
+++ spec/mdb.indexes.js_spec.rb
@@ -3,9 +3,19 @@ require_relative 'spec_helper'
 describe 'mdb.indexes.js' do
   let(:lib_path) { File.expand_path('../../lib/mdb.indexes.js', __FILE__) }
 
-  describe 'function ls_indexes_size_desc' do
-    specify 'returns the list of index size per collection sorted by size desc' do
-      puts run_mongo 'typeof(ls_indexes_size_desc);'
+  describe 'function total_index_size_per_collection()' do
+    specify 'gets defined in mongo shell' do
+      exec = run_mongo 'typeof(total_index_size_per_collection);'
+
+      exec.should eq 'function'
+    end
+
+    specify 'returns the list of index sizes per collection sorted by size desc' do
+       run_mongo('var objects = []; for(var i = 0; i < 1000; ++i) { objects.push({ key: i }); }; db.test.insert(objects); db.test.ensureIndex( { key: 1 } );')
+
+       exec = run_mongo 'total_index_size_per_collection();'
+
+       p exec
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,18 +25,29 @@ RSpec.configure do |conf|
 
   conf.before(:all) {
     FileUtils.mkdir_p $db_settings[:tmp_dbpath]
-    system "#{$db_settings.mdb_path}/mongod --dbpath #{$db_settings[:tmp_dbpath]} --logpath /dev/null --fork"
+    system "#{$db_settings.mdb_path}/mongod --dbpath #{$db_settings[:tmp_dbpath]} --logpath /tmp/test.log --fork"
   }
 
   conf.after(:all) {
     Process.kill 9, File.read($db_settings[:tmp_mongolock]).strip.to_i
+    # FileUtils.rm_rf $db_settings[:tmp_dbpath]
   }
 end
 
 module MongolitaTestHelpers
-  def run_mongo test_execution_str
-    cmd = %Q{#{$db_settings.mdb_path}/mongo --eval "load('#{lib_path}'); #{test_execution_str};"}
-    %x/#{cmd}/
+  @@shell_strip = /MongoDB shell version:.*?connecting to: test/m
+
+  def run_mongo exec_script
+    cmd = %Q{#{$db_settings.mdb_path}/mongo --eval "load('#{lib_path}'); #{exec_script};"}
+    @response = %x/#{cmd}/
+    strip_noise
+  end
+
+  private
+
+  def strip_noise
+    @response[@@shell_strip] = '' if @response.match @@shell_strip
+    @response.strip
   end
 end
 
